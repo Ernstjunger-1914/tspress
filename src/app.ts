2cf2357d9670330675b25ee6a482688a9ca68768
손소독 <vot768@naver.com>
2022-05-01 21:57:49 +0900

    1. api router 추가

--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,13 @@ const app = express();
 const port = process.env.PORT || 3333;
 
 const indexRouter = require('./routes/index');
+const apiRouter = require('./routes/api');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', indexRouter);
+app.use('/api', apiRouter);
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.json({ "status": "server is running" });
+    res.json({ "status": "API" });
 });
 
 module.exports = router;
