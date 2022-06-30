67b339e573d739cb9a057b1948bc1b6f9e166b1b
asdf <vot768@naver.com>
2022-06-30 09:58:12 +0900

    [update] 1. middleware 파일 이름 변경 2. 라우터 controller 내 path 지정하는 방식 변경

--- src/routes/controllers/api.ts
+++ src/routes/controllers/api.ts
@@ -1,13 +1,12 @@
 import { Router } from 'express';
-import { apiLogic } from '../middlewares/apiMiddlewares';
+import { apiLogic } from '../middlewares/apiMiddleware';
 
 const router: Router = Router();
 const middleWare = new apiLogic();
 
-const apiRouterPath = {
-    main: '/'
-};
-
-router.get(apiRouterPath.main, middleWare.get);
+/**
+ * @description path: /api
+ */
+router.get('/', middleWare.get);
 
 export default router;
--- src/routes/controllers/index.ts
+++ src/routes/controllers/index.ts
@@ -1,13 +1,12 @@
 import { Router } from 'express';
-import { indexLogic } from '../middlewares/indexMiddlewares';
+import { indexLogic } from '../middlewares/indexMiddleware';
 
 const router: Router = Router();
 const middleWare = new indexLogic();
 
-const indexRouterPath = {
-    main: '/'
-};
-
-router.get(indexRouterPath.main, middleWare.get);
+/**
+ * @description path: /
+ */
+router.get('/', middleWare.get);
 
 export default router;
