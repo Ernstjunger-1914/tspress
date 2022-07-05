09e01ab115d1a607b1490bb65bcf88802e897338
asdf <vot768@naver.com>
2022-07-05 17:14:58 +0900

    [update] 1. api middleware response에 statusCode 추가

--- src/routes/middlewares/apiMiddleware.ts
+++ src/routes/middlewares/apiMiddleware.ts
@@ -7,6 +7,7 @@ export class apiRouterMethod {
             hostname: req.hostname,
             path: req.originalUrl,
             method: req.method,
+            status: res.statusCode,
             body: req.body
         });
         Log.i(`${req.method} ${req.originalUrl} ${res.statusCode} ${res.statusMessage} ${JSON.stringify(req.route)} \nbody: ${JSON.stringify(req.body)}`);
